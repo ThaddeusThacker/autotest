62a00fe306bd463adf6611b41fbeb11f2acebd17
gabriela.rojas <gabyrocas05@gmail.com>
2016-04-19 22:23:32 -0600

    font copy test (Pablo)

---
+++ automation/src/main/java/com/mazdausa/test/automation/cases/FontHeadlineTest.java
@@ -0,0 +1,44 @@
+package com.mazdausa.test.automation.cases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class FontHeadlineTest  extends BaseTest{
+
+    private WebElement element;
+
+    public FontHeadlineTest(WebDriver driver) {
+        this.setDriver(driver);
+    }
+    public Boolean FontHeadlineTest (int searchContext, String contextValue){
+        Boolean testResult = false;
+        try{
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+            SearchContext myContext = new SearchContext(searchContext, contextValue);
+            element = getWebElement(myContext);
+
+            if(element.getCssValue("font-family").equals("interstate-light,interstatemazda-light,sans-serif,arial")
+                    ||element.getCssValue("color").equals("#fff" )
+                    ||element.getCssValue("font-size").equals("3em" )) {;
+
+                testResult = true;
+
+            }
+
+        }catch(Exception e){
+
+            System.out.println(e.getMessage());
+
+        }
+
+        return testResult;
+
+    }
+
+}
