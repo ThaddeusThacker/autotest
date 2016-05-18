9813f688746499965363893007b0bf6deeddbcc2
gabriela.rojas <gabyrocas05@gmail.com>
2016-05-18 08:00:47 -0600

    Exterior color card test - complete

--- automation/src/main/java/com/mazdausa/test/automation/cases/ExteriorColorTest.java
+++ automation/src/main/java/com/mazdausa/test/automation/cases/ExteriorColorTest.java
@@ -1,10 +1,10 @@
 package com.mazdausa.test.automation.cases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
 import java.util.ArrayList;
 
 
@@ -16,19 +16,21 @@ public class ExteriorColorTest extends BaseTest {
     private ArrayList<String> chips_names;
     private WebElement label_element;
     private ArrayList<String> chips_labels;
+    private WebElement image_wrapper;
 
     public ExteriorColorTest(WebDriver webDriver) {
 
         this.setDriver(webDriver);
     }
 
-    public boolean prepareTests(SearchContext searchContext, ArrayList<String> names,ArrayList<String> labels){
+    public boolean prepareTests(SearchContext searchContext, ArrayList<String> names,ArrayList<String> labels,SearchContext image_wrapper_context ){
         //Receive chips list and parent node
         boolean result = false;
         text_output = "PREPARING TEST: \n";
         chips_names = names;
         chips_labels = labels;
         parent_element = getWebElement(searchContext);
+        image_wrapper = getWebElement(image_wrapper_context);
         if (parent_element != null) {
             chips = (ArrayList<WebElement>) parent_element.findElements(By.className("option"));
             text_output += "chips loaded \n";
@@ -90,7 +92,7 @@ public class ExteriorColorTest extends BaseTest {
             String hoveredPropertyValue = chip.getCssValue("background-position");
             if(initialPropertyValue.equals(hoveredPropertyValue) && !chip.getAttribute("class").contains("selected")){
                 text_output += "Can't detect red ring: " +chip.getAttribute("data-analytics-cta-detail")+ "  \n";
-                testResult = false;
+                test_result = false;
             }
             //Test label copy
 //            if(label_element!= null) {
@@ -107,15 +109,59 @@ public class ExteriorColorTest extends BaseTest {
     public Boolean testChipsImageUpdate(){
         //Receive chips list, parent node, image names
         //For each chip in list, click element and test image update.
-        return true;
-
+        text_output += "TESTING CHIPS IMAGE UPDATE \n";
+        Boolean test_result = true;
+        for(WebElement chip: chips){
+            chip.click();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            WebElement image = image_wrapper.findElement(By.tagName("img"));
+            if(image!=null){
+                String source = image.getAttribute("src");
+                WebElement tag_name = chip.findElement(By.tagName("span"));
+                if (tag_name != null) {
+                    String color_id = tag_name.getAttribute("class");
+                    color_id = color_id.substring(6,color_id.length()-1);
+                    if (!source.toLowerCase().contains(color_id.toLowerCase())) {
+                        test_result = false;
+                        text_output += "Wrong image for chip: " + color_id + "  \n";
+                    }else{
+                        text_output += "Image found for: " + color_id + "  \n";
+                    }
+                }
+            }
+        }
+        return test_result;
     }
 
     public Boolean testChipsLayout(){
         //Receive chips list, parent node
         //For each chip in list, calculate position and validate order.
-        return true;
-
+        text_output += "TESTING CHIPS LAYOUT \n";
+        Boolean test_result = true;
+        Boolean first_time = true;
+        int prev_x = 0;
+        int prev_y = 0;
+        for(WebElement chip: chips){
+            Point point = chip.getLocation();
+            if(first_time){
+                prev_x = point.getX();
+                prev_y = point.getY();
+                first_time = false;
+            }else{
+                if(point.getY() == prev_y && point.getX()>prev_x){
+                    prev_x = point.getX();
+                    prev_y = point.getY();
+                }else {
+                    test_result = false;
+                    break;
+                }
+            }
+        }
+        return test_result;
     }
 
 
