56a6f367ad8be7cd20114ff52adeef59811cf2be
gabriela.rojas <gabyrocas05@gmail.com>
2016-05-26 14:41:24 -0600

    Nav dots Card test

--- automation/src/main/java/com/mazdausa/test/automation/cases/DotsVerificationTest.java
+++ automation/src/main/java/com/mazdausa/test/automation/cases/DotsVerificationTest.java
@@ -14,12 +14,9 @@ public class DotsVerificationTest extends BaseTest {
     private Properties props;
     private String Code;
     private ArrayList<WebElement> dots;
-    //private WebElement parent_element;
     private String text_output;
-    private ArrayList<String> dots_names;
-    private WebElement label_element;
-    // private ArrayList<String> chips_labels;
-    // private WebElement image_wrapper;
+    private ArrayList<String> dots_labels;
+
 
 
     public DotsVerificationTest(WebDriver webDriver, Properties p, String carCode) {
@@ -29,25 +26,18 @@ public class DotsVerificationTest extends BaseTest {
 
     }
 
-    public boolean prepareTests(SearchContext searchContext, ArrayList<String> names){
+    public boolean prepareTests(SearchContext searchContext, ArrayList<String> labels){
         //,ArrayList<String> labels){
-        //Receive chips list and parent node
+        //Receive dots list and parent node
         boolean result = false;
         text_output = "PREPARING TEST: \n";
-        dots_names = names;
-        //chips_labels = labels;
+        dots_labels = labels;
         parent = getWebElement(searchContext);
-        //image_wrapper = getWebElement(image_wrapper_context);
+
         if (parent != null) {
-            dots = (ArrayList<WebElement>) parent.findElements(By.className(","));
+            dots = (ArrayList<WebElement>) parent.findElements(By.tagName("li"));
             text_output += "dots copy loaded \n";
-            label_element = parent.findElement(By.className("option-text")).findElement(By.tagName("span"));
-            if(label_element != null){
-                result = true;
-            }else{
-                text_output += "label element not found \n";
-                result = false;
-            }
+            result = true;
         }else{
             text_output += "parent element not found \n";
             result = false;
@@ -55,70 +45,130 @@ public class DotsVerificationTest extends BaseTest {
         return result;
     }
 
-    public Boolean test(SearchContext searchContext) {
-        Boolean testResult = true;
-        String CarCopy = props.getProperty(Code + "_dots_copy");
-        String CarCount =  props.getProperty(Code + "_dots_count");
-        // CarCopy is a copy list declared on properties file and it will be the copy variable for each dots
+    public String getTextOutput(){
+
+        return text_output;
+    }
+
+    public Boolean testCountDots() {
+        Boolean test_result = true;
+        int car_count =  Integer.parseInt(props.getProperty(Code + "_dots_count"));
         // CarCount is the number of the dots declared on properties
-        try
-        {
-            parent = getWebElement(searchContext);
-            ArrayList<WebElement> arrayDots = (ArrayList<WebElement>) parent.findElements(By.tagName("li"));
-            if (arrayDots.size() != Integer.parseInt(CarCount))
-            {
-                testResult = false;
-            }
-
-
-
-//            else
-//            {
-//                for (int i = 0; i < arrayDots.size(); i++)
-//                {
-//                    if (arrayDots.get(i).getCssValue("cursor").equals("pointer"))
-//                    {
-//                        testResult = true;
-//                        if (arrayDots.get(i).getText() == props.getProperty("3h_dots_copy"))
-//                        {
-//
-//                        }
-//
-//                    }
-//
-//                }
-//            }
-
+        if(dots.size() != car_count){
+            test_result = false;
         }
-        catch(Exception ex)
-        {
-            System.out.println(ex.getMessage());
-            testResult = false;
-        }
-        return testResult;
+        return test_result;
     }
 
-    public Boolean test(int searchContext, String contextValue, String cssProperty){
-        testResult = false;
-        try
-        {
-            SearchContext context = new SearchContext(searchContext, contextValue);
-            parent = getWebElement(context);
-            String initialPropertyValue = parent.getCssValue(cssProperty);
+    public Boolean testDotsHover() {
+        //For each dot in list, hover element and test background color and right copy.
+        text_output += "TESTING DOTS HOVER \n";
+        Boolean test_result = true;
+        String initial_background_value;
+        String hovered_background_value;
+        String dot_label;
+        int label_count = 0;
+        WebElement tooltip;
+        for(WebElement dot: dots){
+            //Obtain the background color
+            initial_background_value = dot.getCssValue("background-color");
+            //Hover
             Actions action = new Actions(driver);
-            action.moveToElement(parent).build().perform();
-            Thread.sleep(100);
-            String hoveredPropertyValue = parent.getCssValue(cssProperty);
-            if(!initialPropertyValue.equals(hoveredPropertyValue)){
-                testResult = true;
+            action.moveToElement(dot).build().perform();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //Obtain the hovered background color
+            hovered_background_value = dot.getCssValue("background-color");
+            //Check background color
+            if(!initial_background_value.equals(hovered_background_value)){
+                test_result = false;
+            }
+            //Check cursor pointer
+            if(!dot.getCssValue("cursor").equals("pointer")){
+                test_result = false;
+            }
+            //Check label visible
+            tooltip = dot.findElement(By.className("tooltip"));
+            if(!tooltip.isDisplayed()){
+                test_result = false;
+            }
+            //Check label copy
+            dot_label = dot.findElement(By.className("text")).getText();
+            if(!dot_label.equals(dots_labels.get(label_count))){
+                test_result = false;
+            }
+            label_count++;
+        }
+        return test_result;
+    }
+
+    public Boolean testDotsClick() {
+        //For each dot in list, click element
+        //Check dot URL
+        text_output += "TESTING DOTS CLICK \n";
+        Boolean test_result = true;
+        for(WebElement dot: dots){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dot.findElement(By.tagName("a")).click();
+            String hash = driver.getCurrentUrl().substring(driver.getCurrentUrl().indexOf('#'));
+            if(!hash.equals(dot.findElement(By.tagName("a")).getAttribute("href"))){
+                test_result = false;
             }
         }
-        catch(Exception ex)
-        {
-            System.out.println(ex.getMessage());
-
-        }
-
-        return testResult;
+        return test_result;
     }
+
+    public Boolean testDotsScroll() {
+        //For each dot in list, click element
+        //Check dot URL
+        text_output += "TESTING DOTS SCROLL \n";
+        Boolean test_result = true;
+        String panel_id;
+        WebElement panel;
+        WebElement tooltip;
+        String initial_background_value;
+        String hovered_background_value;
+        for(WebElement dot: dots){
+            initial_background_value = dot.getCssValue("background-color");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            panel_id = dot.findElement(By.tagName("a")).getAttribute("href");
+            panel_id = panel_id.substring(panel_id.indexOf("#") + 1);
+            panel = driver.findElement(By.id(panel_id));
+            if(panel != null){
+                Actions action = new Actions(driver);
+                action.moveToElement(panel).build().perform();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //Check label visible
+                tooltip = dot.findElement(By.className("tooltip"));
+                if(!tooltip.isDisplayed()){
+                    test_result = false;
+                }
+                //Check background color
+                hovered_background_value = dot.getCssValue("background-color");
+                if(!initial_background_value.equals(hovered_background_value)){
+                    test_result = false;
+                }
+            }else{
+                text_output += "unable to find panel:" + panel_id + " \n";
+                test_result = false;
+            }
+        }
+        return test_result;
+    }
+
 }
