3fc19f1d9c05e644830ad7effce2475a98d4d3c4
dellys.guillen <dellys.guillen@gmail.com>
2016-06-13 13:14:44 -0600

    TabVerificationTest Verify new window

--- automation/src/main/java/com/mazdausa/test/automation/cases/TabVerificationTest.java
+++ automation/src/main/java/com/mazdausa/test/automation/cases/TabVerificationTest.java
@@ -29,29 +29,33 @@ public class TabVerificationTest extends BaseTest {
 
 
     public Boolean test(int searchContext, String contextValue, String targetURL) {
+        //window identification
         String mwh = driver.getWindowHandle();
+
         try {
-
             SearchContext clickdropdownLink = new SearchContext(searchContext, contextValue);
-
-            driver.findElement(By.id("nav-shop")).click();
             element = getWebElement(clickdropdownLink);
             element.click();
+            // opens identifications
             Set wins = driver.getWindowHandles();
+            // iterate windows
             Iterator ite = wins.iterator();
-
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
+            Thread.sleep(2500);
+            // verify the windows
             while(ite.hasNext())
             {
                 String popupHandle=ite.next().toString();
                 if(!popupHandle.contains(mwh))
                 {
                     driver.switchTo().window(popupHandle);
+                    if (targetURL.compareTo(driver.getCurrentUrl()) == 0) {
+                        testResult = true;
+                    } else {
+                        testResult = false;
+                        System.out.println("Target URL = " + targetURL);
+                        System.out.println("Current URL = " + driver.getCurrentUrl());
+
+                    }
                     driver.switchTo().window(mwh);
                 }
             }
